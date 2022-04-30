eba55bbad8e5475a398d5ffeae4e63e995e5fc98
leo hsieh <bardiche4768@gmail.com>
2022-04-30 16:12:11 +0800

    enable x11 forwarding

---
+++ launch_image.sh
@@ -0,0 +1,26 @@
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+xhost +local:root;
+nvidia-docker run \
+    --net host --privileged \
+    -v $HOME:/shared -e \
+    DISPLAY=$DISPLAY -e \
+    LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v $HOME/ros_noetic_docker/catkin_ws:/catkin_ws:rw \
+    -v $HOME/ros_noetic_docker/data:/data:ro \
+    -it ros_noetic_docker
+
+    
+echo "exiting docker"

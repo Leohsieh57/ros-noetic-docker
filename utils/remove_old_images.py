5f06c08e902d733f0e72163cb12ee457e46eaa5b
leo hsieh <bardiche4768@gmail.com>
2022-05-01 23:44:38 +0800

    update img remover

--- utils/remove_old_images.py
+++ utils/remove_old_images.py
@@ -9,6 +9,11 @@ if __name__ == "__main__":
     lines = open(txt_path).readlines()
     lines = [[x for x in line.split("  ") if x] for line in lines]
     for image_id in [line[2] for line in lines]: 
+        while image_id[-1] == ' ': 
+            image_id = image_id[:-1]
+        while image_id[0] == ' ': 
+            image_id = image_id[1:]
+
         assert len(image_id) == 12
         os.system("docker image rm -f %s"%image_id)
 
